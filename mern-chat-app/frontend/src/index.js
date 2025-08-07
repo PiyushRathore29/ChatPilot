3a5be3209905c1d97f24b1cadeb2fa085e57bfe0
Piyush Raj <rathorepiyush221@gmail.com>
2025-08-07 20:24:35 +0530

    sorted the fronted issues

--- mern-chat-app/frontend/src/index.js
+++ mern-chat-app/frontend/src/index.js
@@ -17,6 +17,7 @@ ReactDOM.render(
   </ChakraProvider>,
   document.getElementById("root")
 );
+//00
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
